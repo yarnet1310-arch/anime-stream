70b53c30074f0cb733458b626223857968fb0195
yarnet1310-arch <yarnet1310@gmail.com>
2026-04-21 09:16:05 +0700

    Update Review.ts

--- backend/src/models/Review.ts
+++ backend/src/models/Review.ts
@@ -1,1 +1,21 @@
+import mongoose, { Schema, Document } from 'mongoose';
 
+interface IReview extends Document {
+  user: string;
+  animeId: string;
+  animeTitle: string;
+  rating: number;
+  comment: string;
+  createdAt: Date;
+}
+
+const ReviewSchema: Schema = new Schema({
+  user: { type: String, required: true },
+  animeId: { type: String, required: true },
+  animeTitle: { type: String, required: true },
+  rating: { type: Number, required: true },
+  comment: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+export default mongoose.model<IReview>('Review', ReviewSchema);
