218628d4387aa4655a044bec959c776e98e33223
yarnet1310-arch <yarnet1310@gmail.com>
2026-04-21 08:59:43 +0700

    Set up routing for main application pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,1 +1,25 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Search from './pages/Search';
+import Watch from './pages/Watch';
+import Reviews from './pages/Reviews';
+import Watchlist from './pages/Watchlist';
 
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/watch/:id" element={<Watch />} />
+        <Route path="/reviews" element={<Reviews />} />
+        <Route path="/watchlist" element={<Watchlist />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
